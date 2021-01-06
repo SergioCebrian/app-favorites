e61bcd93e7cde451255a495f5746d5586ad27a22
SergioCebrian <sergiocebrian85@gmail.com>
2021-01-06 20:31:04 +0100

    v1.0.0

--- src/app/core/mocks/category.mock.ts
+++ src/app/core/mocks/category.mock.ts
@@ -3,6 +3,7 @@ export const CategoryMock = [
         id: '1',
         title: 'Title 1',
         description: 'Description 1',
+        image: 'image1.jpg',
         type: 'Type 1',
         slug: 'Slug 1',
         lastModifiedDate: new Date()
@@ -11,6 +12,7 @@ export const CategoryMock = [
         id: '2',
         title: 'Title 2',
         description: 'Description 2',
+        image: 'image2.jpg',
         type: 'Type 2',
         slug: 'Slug 2',
         lastModifiedDate: new Date()
--- src/app/core/services/upload/upload.service.spec.ts
+++ src/app/core/services/upload/upload.service.spec.ts
@@ -1,13 +1,41 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule } from '@ngrx/store';
+import { appReducers } from '@shared/store/reducers/app.reducers';
+import { environment } from '@environments/environment';
+import { SlugService } from '@helpers/slug/slug.service';
+
+import { CategoryModel } from '@models/category.model';
+import { CategoryMock } from '@mocks/category.mock';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 
 import { UploadService } from './upload.service';
 
 describe('UploadService', () => {
   let service: UploadService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        HttpClientTestingModule,
+        RouterTestingModule, 
+        StoreModule.forRoot(appReducers)
+      ],
+      providers: [ 
+        SlugService,
+        AngularFirestore
+      ]
+    });
     service = TestBed.inject(UploadService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
