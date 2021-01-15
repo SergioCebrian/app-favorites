9974036b50dd1eecfe3728056be89e999572f349
SergioCebrian <sergiocebrian85@gmail.com>
2021-01-15 18:12:56 +0100

    v1.0.0

--- src/app/modules/categories/components/categories-list/categories-list.component.spec.ts
+++ src/app/modules/categories/components/categories-list/categories-list.component.spec.ts
@@ -1,8 +1,12 @@
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ICategory } from '@interfaces/category';
 import { IonicModule } from '@ionic/angular';
 import { CategoryMock } from '@mocks/category.mock';
 import { CategoryModel } from '@models/category.model';
+import { selectCategory } from '@modules/categories/store/selectors/categories.selectors';
 
 import { CategoriesListComponent } from './categories-list.component';
 
@@ -10,6 +14,9 @@ describe('CategoriesListComponent', () => {
   let component: CategoriesListComponent;
   let fixture: ComponentFixture<CategoriesListComponent>;
   let expectedCategories: CategoryModel[];
+  let expectedCategory: ICategory;
+  let itemDe: DebugElement;
+  let itemEl;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,7 +30,10 @@ describe('CategoriesListComponent', () => {
     fixture = TestBed.createComponent(CategoriesListComponent);
     component = fixture.componentInstance;
     expectedCategories = CategoryMock;
+    expectedCategory = { ...expectedCategories[0], createdDate: new Date() };
     component.allCategories = expectedCategories;
+    //itemDe = fixture.debugElement.query(By.css('#test'));
+    //itemEl = itemDe.nativeElement;
     fixture.detectChanges();
   }));
 
@@ -40,6 +50,14 @@ describe('CategoriesListComponent', () => {
     it('Testing count categories', () => {
       expect(component.allCategories.length).toEqual(2);
     });
+
+   /*it('Testing delete a category', () => {
+      let selectedCategory;
+      component.OnDeleteCategory.subscribe((category) => selectedCategory = category);
+      itemDe.triggerEventHandler('click', null);
+      expect(selectedCategory.id).toBe('1');
+      // expect(selectedCategory).toEqual(expectedCategory);
+    });*/
 
   });
 
